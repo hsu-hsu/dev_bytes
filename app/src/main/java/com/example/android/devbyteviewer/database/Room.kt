c8a9bb99153693f2770b0a59a3529fc3471b98c0
hsu-hsu <hsuyethin@gmail.com>
2023-09-25 13:09:22 +0700

    load from network and store database as cache and retrieve from datadase and adjust dependencies

--- app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -26,34 +26,29 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import kotlinx.coroutines.selects.select
 
 @Dao
-interface VideoDao{
-
+interface VideoDao {
     @Query("select * from databasevideo")
-    fun getVideo(): LiveData<List<DatabaseVideo>>
+    fun getVideos(): LiveData<List<DatabaseVideo>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(vararg videos: DatabaseVideo)
-
 }
 
 @Database(entities = [DatabaseVideo::class], version = 1)
-abstract class VideoDatabase: RoomDatabase() {
+abstract class VideosDatabase : RoomDatabase() {
     abstract val videoDao: VideoDao
 }
 
-private lateinit var INSTANCE : VideoDatabase
+private lateinit var INSTANCE: VideosDatabase
 
-fun getDatabase(context: Context): VideoDatabase {
-    synchronized(VideoDatabase::class.java) {
-        if(!::INSTANCE.isInitialized) {
-            INSTANCE = Room.databaseBuilder(
-                context.applicationContext,
-                VideoDatabase::class.java,
-                "videos"
-            ).build()
+fun getDatabase(context: Context): VideosDatabase {
+    synchronized(VideosDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(context.applicationContext,
+                VideosDatabase::class.java,
+                "videos").build()
         }
     }
     return INSTANCE
